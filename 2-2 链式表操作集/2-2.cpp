6413f4638a96e09c2303da8b0557592c58851b4a
LittleStar <192354134@qq.com>
2021-09-26 22:12:51 +0800

    2-1 逆序数据建立链表

---
+++ 2-2 链式表操作集/2-2.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define ERROR NULL
+typedef int ElementType;
+typedef struct LNode* PtrToLNode;
+struct LNode {
+	ElementType Data;
+	PtrToLNode Next;
+};
+typedef PtrToLNode Position;
+typedef PtrToLNode List;
+
+Position Find(List L, ElementType X);
+List Insert(List L, ElementType X, Position P);
+List Delete(List L, Position P);
+
+int main()
+{
+	List L;
+	ElementType X;
+	Position P, tmp;
+	int N;
+
+	L = NULL;
+	scanf("%d", &N);
+	while (N--) {
+		scanf("%d", &X);
+		L = Insert(L, X, L);
+		if (L == ERROR) printf("Wrong Answer\n");
+	}
+	scanf("%d", &N);
+	while (N--) {
+		scanf("%d", &X);
+		P = Find(L, X);
+		if (P == ERROR)
+			printf("Finding Error: %d is not in.\n", X);
+		else {
+			L = Delete(L, P);
+			printf("%d is found and deleted.\n", X);
+			if (L == ERROR)
+				printf("Wrong Answer or Empty List.\n");
+		}
+	}
+	L = Insert(L, X, NULL);
+	if (L == ERROR) printf("Wrong Answer\n");
+	else
+		printf("%d is inserted as the last element.\n", X);
+	P = (Position)malloc(sizeof(struct LNode));
+	tmp = Insert(L, X, P);
+	if (tmp != ERROR) printf("Wrong Answer\n");
+	tmp = Delete(L, P);
+	if (tmp != ERROR) printf("Wrong Answer\n");
+	for (P = L; P; P = P->Next) printf("%d ", P->Data);
+	return 0;
+}
+
+/* 你的代码将被嵌在这里 */
