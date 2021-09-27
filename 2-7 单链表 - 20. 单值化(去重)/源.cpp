ad1f237ffe6731ce2df258f8a5de18e61fec37a4
LittleStar <192354134@qq.com>
2021-09-28 02:44:35 +0800

    2021.9.28

--- 2-7 单链表 - 20. 单值化(去重)/源.cpp
+++ 2-7 单链表 - 20. 单值化(去重)/源.cpp
@@ -1,13 +1,37 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+typedef struct Node* PtrToNode;
+struct Node {
+	ElementType data;
+	PtrToNode   next;
+};
+typedef struct Node LLIST;
+
+void LListUnique(LLIST* list);
 
 int main()
 {
-	LLIST a;
-	LListCreate(&a);
-	LListInput(&a);
-	LListUnique(&a);
-	LListOutput(&a);
-	putchar('\n');
-	LListDestroy(&a);
-	return 0;
+
+}
+void LListUnique(LLIST* list)
+{
+	LLIST* head = list, * save = list;
+	LLIST temp;
+	while (list)
+	{
+		temp.data = list->data; 
+		while (head->next)
+		{
+			if (head->next->data == temp.data)
+			{
+				head->next = head->next->next;
+			}
+			head = head->next;
+		}
+		list = list->next;
+	}
+	printf("结构体大小为 %d", sizeof(LLIST));
+	printf("%f", *(double*)(list + sizeof(LLIST*)));
 }
